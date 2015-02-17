ace47ae2d7cabdbaa47f44d38520e739d4b73c0f
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-17 00:41:17 -0500

    parser-tests: Add more builder tests.

--- hs/TestSVGBuilder.hs
+++ hs/TestSVGBuilder.hs
@@ -8,10 +8,19 @@ testJoinPathTuple = TestList [
 
 testConvertRationalTupToString :: Test
 testConvertRationalTupToString = TestList [
-    
+    ("123.0","456.0") ~=? convertRationalTupToString (123, 456),
+    ("123.123","456.456") ~=? convertRationalTupToString (123.123, 456.456)
+    ]
+
+testBuildPathString :: Test
+testBuildPathString = TestList [
+    "123.0,456.0" ~=? buildPathString [(123,456)],
+    "123.0,456.0 123.123,456.456" ~=? buildPathString [(123, 456), (123.123, 456.456)]
     ]
 
 main :: IO ()
 main = do
          runTestTT testJoinPathTuple
+         runTestTT testConvertRationalTupToString
+         runTestTT testBuildPathString
          return ()
